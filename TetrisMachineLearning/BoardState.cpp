546d597b2fed3546fb00b4b51502708bae2830e9
Emorgas <Indieboi_918@hotmail.com>
2016-02-22 22:08:01 +0000

    BoardState.cpp added

---
+++ TetrisMachineLearning/BoardState.cpp
@@ -0,0 +1,21 @@
+#include "BoardState.h"
+
+
+
+BoardState::BoardState(bool board[BOARD_WIDTH][BOARD_HEIGHT])
+{
+	_rotations = 0;
+	_translation = 0;
+	for (int h = 0; h < BOARD_HEIGHT; h++)
+	{
+		for (int w = 0; w < BOARD_WIDTH; w++)
+		{
+			_gameBoard[w][h] = board[w][h];
+		}
+	}
+}
+
+
+BoardState::~BoardState()
+{
+}
